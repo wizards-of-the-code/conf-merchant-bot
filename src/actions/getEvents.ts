6d686ecf2d0de5115b82f5d437703e83ec4cba76
Lexeor <lexeor.ross@gmail.com>
2023-08-25 13:50:52 +0400

    add try/catch for deleting messages from context

--- src/actions/getEvents.ts
+++ src/actions/getEvents.ts
@@ -6,7 +6,12 @@ import { IBotContext } from '../context/BotContext.interface';
 export const sendEventsMessage = async (bot: TelegramBot, ctx: IBotContext) => {
   const eventsArr: Event[] = await bot.dbManager.getEvents();
 
-  ctx.deleteMessage();
+  try {
+    ctx.deleteMessage();
+  } catch (e) {
+    console.log('Error when trying to delete old message');
+  }
+
   const confs = eventsArr.map((event) => [Markup.button.callback(`${event.location.city}, ${event.location.country}, ${event.datetime}`, `action_get_info_${event._id!.toString()}`)]);
 
   ctx.reply('Какая именно конференция вас интересует?', Markup.inlineKeyboard([
