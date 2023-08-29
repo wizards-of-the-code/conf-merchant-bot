5f0e369d8e4e1bfdf4e3a3973893597e69965e17
Lexeor <lexeor.ross@gmail.com>
2023-08-29 12:53:15 +0400

    add cancelParticipation action

---
+++ src/actions/cancelParticipation.ts
@@ -0,0 +1,43 @@
+import { ObjectId } from 'mongodb';
+import { Markup } from 'telegraf';
+import { Participant } from '../types';
+import TelegramBot from '../TelegramBot';
+
+const cancelParticipation = async (bot: TelegramBot) => {
+  bot.action(/action_cancel_participation/, async (ctx) => {
+    // Get role from actionString
+    const eventId = ctx.session.selectedConf!._id;
+
+    // TODO: Check if event exists
+    const event = await bot.dbManager.getEventById(new ObjectId(eventId));
+
+    // Get participant from DB
+    const participant: Participant | null = await bot.dbManager.getParticipant(ctx.from!.id);
+
+    const removeFromEventResult = await bot
+      .dbManager
+      .removeParticipantFromEvent(new ObjectId(eventId), participant);
+
+    let userMessage: string;
+
+    if (removeFromEventResult.modifiedCount > 0) {
+      // Add event details to Participant entry
+      await bot.dbManager.removeEventDetailsToParticipant(new ObjectId(eventId), participant);
+      // TODO: Handle result of addEventDetailsToParticipant
+
+      userMessage = `Ваша регистрация на конференцию "${event!.name}" успешно отменена.`;
+    } else {
+      userMessage = 'По какой-то причине вы и не были записаны на эту конференцию.';
+    }
+
+    ctx.editMessageReplyMarkup(undefined);
+    ctx.reply(userMessage, Markup.inlineKeyboard(
+      [
+        Markup.button.callback('◀️ Назад', `action_get_info_${eventId}`),
+        Markup.button.callback('🔼 В главное меню', 'action_get_events'),
+      ],
+    ));
+  });
+};
+
+export default cancelParticipation;
