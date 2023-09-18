a86166e9264145f6a0c60f9c2cd7cc33f545d5e3
Lexeor <lexeor.ross@gmail.com>
2023-09-18 17:06:45 +0400

    add session.selectedEvent nullify

--- src/actions/getEvents.ts
+++ src/actions/getEvents.ts
@@ -5,6 +5,8 @@ import Paginator from '../utils/paginator';
 import formatDateToDdMmYyyy from '../utils/dateFormat';
 
 export const sendEventsMessage = async (bot: TelegramBot, ctx: IBotContext) => {
+  ctx.session.selectedEvent = null;
+
   await ctx.deleteMessage().catch(
     (error) => {
       console.error('Error when trying to delete message: ', error);
