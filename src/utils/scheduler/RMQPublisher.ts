266f0a0f1b163ce6b719e3534d1834ddf236c8f0
egatapova <ergatapovaer@gmail.com>
2023-10-03 19:26:13 +0300

    publisher pt2

--- src/utils/scheduler/RMQPublisher.ts
+++ src/utils/scheduler/RMQPublisher.ts
@@ -1,5 +1,10 @@
 import amqp from 'amqplib';
 
+interface MessageObject {
+  recipientId: number,
+  notifiation: Notification,
+}
+
 class RMQPublisher {
   private RMQ_URL: string = 'fjfn';
 
@@ -9,14 +14,31 @@ class RMQPublisher {
 
   private exchangeName: string;
 
-  constuctor(exchangeName: string) {
+  constructor(exchangeName: string) {
     this.exchangeName = exchangeName;
   }
 
   async init() {
     this.connection = await amqp.connect('amqp://your-rabbitmq-host');
     this.channel = await this.connection.createChannel();
-    await this.channel.assertExchange(this.exchangeName, 'fanout', { durable: false });
+    await this.channel.assertQueue(this.exchangeName, { durable: false });
+  }
+
+  async publish(message: MessageObject) {
+    if (!this.channel) {
+      throw new Error('RabbitMQ not initialized!');
+    }
+
+    this.channel.sendToQueue(this.exchangeName, Buffer.from(JSON.stringify(message)));
+  }
+
+  async closeChannel() {
+    if (!this.connection || !this.channel) {
+      throw new Error('RabbitMQ not initialized!');
+    }
+
+    await this.channel.close();
+    await this.connection.close();
   }
 }
 
