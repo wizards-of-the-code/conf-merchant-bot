ed8d0f13730a89ae5019c031ea43fda8f126f712
egatapova <ergatapovaer@gmail.com>
2023-10-04 12:28:12 +0300

    bug

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ class App {
       dbManager,
     );
     this.bot.use((
-      new LocalSession({ database: './sessions/sessions.json' })).middleware());
+      new LocalSession({ database: `${this.configService.get('SESSIONS_PATH')}/sessions.json` })).middleware());
     this.scheduler = new Scheduler('0 0 19 * * *', dbManager, this.bot);
   }
 
